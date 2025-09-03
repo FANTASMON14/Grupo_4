e52b43a4fda483d1da39a80dbd3c891d02125cda
cdqh777 <cdq2108@gmail.com>
2025-09-03 11:21:30 -0400

    Agregar el archivo phyton

---
+++ operacion.py
@@ -0,0 +1,9 @@
+# Operación simple
+numero1 = 5
+numero2 = 10
+suma = numero1 + numero2
+print("La suma de", numero1, "y", numero2, "es:", suma)
+print("1 Oliver Hector Ramos Calle")
+print("2 Carlos Daniel Quispe Huanca")
+print("3 Eddy Rodrigo Cuyuña Kuno")
+print("4 Romel Robinho Quispe Cavo")
