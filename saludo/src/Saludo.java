4b67f2cdfd86b04b049ac9a0876ca8508529f1f4
romelengine <romelrobinhoquispecayo@gmail.com>
2025-09-03 11:09:16 -0400

    se agrega la carpeta java llamado saludo

---
+++ saludo/src/Saludo.java
@@ -0,0 +1,10 @@
+
+public class Saludo {
+    public static void main(String[] args) {
+        System.out.println(" ¡Hola equipo!");
+        System.out.println("Oliver Hector Ramos Calle");
+        System.out.println("Carlos Daniel Quispe Huanca");
+        System.out.println("Eddy Rodgrigo Cuyuña Kuno");
+        System.out.println("Romel Robinho Quispe Cayo");
+    }
+}
